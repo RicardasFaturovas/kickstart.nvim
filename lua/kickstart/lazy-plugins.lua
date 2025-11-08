58cd24f88d4fecf87c5cc8a25be6640c80031e7e
RicardasFaturovas <ricardas.faturovas@gmail.com>
2025-11-08 16:55:14 +0200

    Add fugitive, ts formatting change

--- lua/kickstart/lazy-plugins.lua
+++ lua/kickstart/lazy-plugins.lua
@@ -35,6 +35,9 @@ require('lazy').setup({
 
   require 'kickstart.plugins.harpoon',
 
+  require 'kickstart.plugins.neo-tree',
+
+  require 'kickstart.plugins.fugitive',
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
